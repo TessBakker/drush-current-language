8b75ee8373799174ac613385140a41737b4d6368
Tess Bakker <tess@tessabakker.com>
2024-11-25 11:31:14 +0100

    Set Drush code style

--- CurrentLanguageCommands.php
+++ CurrentLanguageCommands.php
@@ -19,60 +19,65 @@ use Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException;
 
 final class CurrentLanguageCommands extends DrushCommands
 {
-  use AutowireTrait;
+    use AutowireTrait;
 
-  public function __construct(
-    protected BootstrapManager $bootstrapManager,
-    protected LanguageManagerInterface $languageManager,
-    protected LanguageNegotiatorInterface $languageNegotiator,
-    protected TranslationManager $translationManager,
-    protected AccountProxyInterface $currentUser
-  ) { }
+    public function __construct(
+        protected BootstrapManager $bootstrapManager,
+        protected LanguageManagerInterface $languageManager,
+        protected LanguageNegotiatorInterface $languageNegotiator,
+        protected TranslationManager $translationManager,
+        protected AccountProxyInterface $currentUser
+    ) {
+    }
 
-  public function getBootstrapManager(): BootstrapManager {
-    return $this->bootstrapManager;
-  }
+    public function getBootstrapManager(): BootstrapManager
+    {
+        return $this->bootstrapManager;
+    }
 
-  public function getLanguageManager(): LanguageManagerInterface {
-    return $this->languageManager;
-  }
+    public function getLanguageManager(): LanguageManagerInterface
+    {
+        return $this->languageManager;
+    }
 
-  public function getLanguageNegotiator(): LanguageNegotiatorInterface {
-    return $this->languageNegotiator;
-  }
+    public function getLanguageNegotiator(): LanguageNegotiatorInterface
+    {
+        return $this->languageNegotiator;
+    }
 
-  public function getTranslationManager(): TranslationManager {
-    return $this->translationManager;
-  }
+    public function getTranslationManager(): TranslationManager
+    {
+        return $this->translationManager;
+    }
 
-  public function getCurrentUser(): AccountProxyInterface {
-    return $this->currentUser;
-  }
+    public function getCurrentUser(): AccountProxyInterface
+    {
+        return $this->currentUser;
+    }
 
   /**
    * Ensure current language is set.
    */
-  #[CLI\Hook(type: HookManager::PRE_COMMAND_HOOK, target: '*')]
-  public function preCommand(CommandData $commandData) {
-    // Drupal must be fully bootstrapped in order to use this validation.
-    if (!$this->getBootstrapManager()->hasBootstrapped( DrupalBootLevels::FULL)) {
-      return;
-    }
-    try {
-      $languageManager = $this->getLanguageManager();
-      $negotiator = $this->getLanguageNegotiator();
-    }
-    catch (ServiceNotFoundException $exception) {
-      // If we do not have these services, this command is not really useful.
-      return;
-    }
-    $negotiator->setCurrentUser($this->getCurrentUser());
+    #[CLI\Hook(type: HookManager::PRE_COMMAND_HOOK, target: '*')]
+    public function preCommand(CommandData $commandData)
+    {
+      // Drupal must be fully bootstrapped in order to use this validation.
+        if (!$this->getBootstrapManager()->hasBootstrapped(DrupalBootLevels::FULL)) {
+            return;
+        }
+        try {
+            $languageManager = $this->getLanguageManager();
+            $negotiator = $this->getLanguageNegotiator();
+        } catch (ServiceNotFoundException $exception) {
+          // If we do not have these services, this command is not really useful.
+            return;
+        }
+        $negotiator->setCurrentUser($this->getCurrentUser());
 
-    if ($languageManager instanceof ConfigurableLanguageManagerInterface) {
-      $languageManager->setNegotiator($negotiator);
-      $languageManager->setConfigOverrideLanguage($languageManager->getCurrentLanguage());
+        if ($languageManager instanceof ConfigurableLanguageManagerInterface) {
+            $languageManager->setNegotiator($negotiator);
+            $languageManager->setConfigOverrideLanguage($languageManager->getCurrentLanguage());
+        }
+        $this->getTranslationManager()->setDefaultLangcode($languageManager->getCurrentLanguage()->getId());
     }
-    $this->getTranslationManager()->setDefaultLangcode($languageManager->getCurrentLanguage()->getId());
-  }
-
 }
